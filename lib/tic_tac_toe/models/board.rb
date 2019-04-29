850944a28ee11de63f8f154b6d6176f766f7b5ee
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-29 14:23:17 -0700

    refactor such that board take an initial state or row_size parameter

--- lib/tic_tac_toe/models/board.rb
+++ lib/tic_tac_toe/models/board.rb
@@ -1,7 +1,7 @@
 class TicTacToe::Board
   attr_accessor :cells
   
-  def initialize(row_size=3, state=Array.new(row_size**2))
+  def initialize(row_size: 3, state: Array.new(row_size**2))
     self.cells = state
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe TicTacToe::Board do
   end
 
   it 'initializes with an empty array representing a board of arbitrary row size' do
-    expect(TicTacToe::Board.new(5).cells).to eq(Array.new(25))
+    expect(TicTacToe::Board.new(row_size: 5).cells).to eq(Array.new(25))
+  end
+
+  it 'can initialize with a passed-in state' do
+    state = [nil, nil, "X", nil, "O", nil, "X", nil, nil]
+    expect(TicTacToe::Board.new(state: state).cells).to eq(state)
   end
 end
