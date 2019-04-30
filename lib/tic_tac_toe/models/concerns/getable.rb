cce27427a1ba5e2b49688541cdb4be4a6410a7fc
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-30 14:07:18 -0700

    add method to get user input and return a formatted move

--- lib/tic_tac_toe/models/concerns/getable.rb
+++ lib/tic_tac_toe/models/concerns/getable.rb
@@ -1,7 +1,14 @@
 module TicTacToe::IO
   module Getable
+    include TicTacToe::Formattable
+
     def get_input
       gets.chomp
+    end
+
+    def get_move
+      input = get_input
+      format_move(input)
     end
   end
 end
