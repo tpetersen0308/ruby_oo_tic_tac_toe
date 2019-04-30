45cdc07b44f801897dbaa8600f4d0571ad5680a5
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-29 17:01:59 -0700

    add method to check if the game is won

---
+++ lib/tic_tac_toe/models/concerns/checkable.rb
@@ -0,0 +1,43 @@
+module TicTacToe::Checkable
+  def self.won?(board)
+    check_row_win(board) || check_column_win(board) || check_diagonal_win(board)
+  end
+
+  private
+    def self.check_row_win(board)
+      rows = get_rows(board)
+      check_combos(rows)
+    end
+
+    def self.check_column_win(board)
+      columns = get_columns(board)
+      check_combos(columns)
+    end
+
+    def self.check_diagonal_win(board)
+      row_size = Math.sqrt(board.size)
+      nw_to_se_cells = (0..(row_size - 1).to_i).map do |n|
+        board[n * row_size + n]
+      end
+      sw_to_ne_cells = (0..(row_size - 1).to_i).map do |n|
+        board[n * (row_size - 1) + 2]
+      end
+
+      check_combos([nw_to_se_cells, sw_to_ne_cells])
+    end
+
+    def self.check_combos(combos)
+      combos.any? do |combo|
+        combo.compact.size == combo.size && combo.uniq.size == 1
+      end
+    end
+
+    def self.get_rows(board)
+      row_size = Math.sqrt(board.size)
+      board.each_slice(row_size).to_a
+    end
+
+    def self.get_columns(board)
+      get_rows(board).transpose
+    end
+end
---
+++ spec/checkable_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+RSpec.describe TicTacToe::Checkable do
+  [
+    [0, 1, 2, "top row"],
+    [3, 4, 5, "middle row"],
+    [6, 7, 8, "bottom row"],
+    [0, 3, 6, "left column"],
+    [1, 4, 7, "middle column"],
+    [2, 5, 8, "right column"],
+    [0, 4, 8, "NW to SE diagonal"],
+    [2, 4, 6, "SW to NE diagonal"]
+  ].each do |first, second, third, desc|
+    it "can check for a #{desc} win" do
+      board = Array.new(9)
+      board[first], board[second], board[third] = "X", "X", "X"
+      
+      expect(TicTacToe::Checkable.won?(board)).to be_truthy
+    end
+  end
+
+  it "returns a falsy value when there is not a win" do
+    board = [nil, nil, "X", nil, "O", nil, "X", nil, nil]
+
+    expect(TicTacToe::Checkable.won?(board)).to be_falsy
+  end
+end
