4b3af5ec5781ecc2d944c25165938701a02a538f
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-30 10:07:38 -0700

    add dependency injection to computer player so it can find an available space on the board

--- lib/tic_tac_toe/models/computer_player.rb
+++ lib/tic_tac_toe/models/computer_player.rb
@@ -1,5 +1,12 @@
 class TicTacToe::ComputerPlayer < TicTacToe::Player
-  def move(board)
-    board.available_cells.sample
+  attr_reader :board_reader
+
+  def initialize(token, board_reader: nil)
+    super(token)
+    @board_reader = board_reader
+  end
+  
+  def move
+    self.board_reader.available_cells.sample
   end
 end
