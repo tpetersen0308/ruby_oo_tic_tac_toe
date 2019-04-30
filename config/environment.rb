e6de94761a643b702a92242e00f4ea4df843e6ab
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-30 13:04:39 -0700

    require files for new modules

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,9 @@ Bundler.require
 
 require_relative '../lib/tic_tac_toe/models/concerns/tic_tac_toe.rb'
 require_relative '../lib/tic_tac_toe/models/concerns/checkable.rb'
+require_relative '../lib/tic_tac_toe/models/concerns/formattable.rb'
+require_relative '../lib/tic_tac_toe/models/concerns/getable.rb'
+require_relative '../lib/tic_tac_toe/models/concerns/printable.rb'
 require_relative '../lib/tic_tac_toe/models/board.rb'
 require_relative '../lib/tic_tac_toe/models/player.rb'
 require_relative '../lib/tic_tac_toe/models/computer_player.rb'
