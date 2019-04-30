0075c71256e48245783a96e05066639036d2a804
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-29 17:02:11 -0700

    require checkable module

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,5 @@ require 'bundler'
 Bundler.require
 
 require_relative '../lib/tic_tac_toe/models/concerns/tic_tac_toe.rb'
+require_relative '../lib/tic_tac_toe/models/concerns/checkable.rb'
 require_relative '../lib/tic_tac_toe/models/board.rb'
