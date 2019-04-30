e5c656e5f0034598b6b34286b063c2844bac7cba
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-30 09:44:47 -0700

    require new class files in environment

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ Bundler.require
 require_relative '../lib/tic_tac_toe/models/concerns/tic_tac_toe.rb'
 require_relative '../lib/tic_tac_toe/models/concerns/checkable.rb'
 require_relative '../lib/tic_tac_toe/models/board.rb'
+require_relative '../lib/tic_tac_toe/models/player.rb'
+require_relative '../lib/tic_tac_toe/models/computer_player.rb'
