7d3ba24d1fff5e09a482e5adac7f12f33c8385e3
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-30 10:25:45 -0700

    store dependency in instance variable instead of setting it as an attribute

--- lib/tic_tac_toe/models/computer_player.rb
+++ lib/tic_tac_toe/models/computer_player.rb
@@ -1,12 +1,10 @@
 class TicTacToe::ComputerPlayer < TicTacToe::Player
-  attr_reader :board_reader
-
   def initialize(token, board_reader: nil)
     super(token)
     @board_reader = board_reader
   end
   
   def move
-    self.board_reader.available_cells.sample
+    @board_reader.available_cells.sample
   end
 end
