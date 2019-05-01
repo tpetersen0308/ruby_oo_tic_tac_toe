a3114101def57616d137505fc53e730c82120489
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-01 12:55:57 -0700

    add reader method for ui attr

---
+++ lib/human_player.rb
@@ -0,0 +1,16 @@
+class TicTacToe::HumanPlayer < TicTacToe::Player
+  attr_reader :ui
+  
+  def initialize(token, ui: nil)
+    super(token)
+    @ui = ui
+  end
+
+  def move
+    ui.get_input
+  end
+
+  def human?
+    true
+  end
+end
