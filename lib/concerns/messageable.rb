c50e4424a8c44599183822fa5317393daa8e4edb
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-01 16:11:21 -0700

    create module to set up a new game

--- lib/concerns/messageable.rb
+++ lib/concerns/messageable.rb
@@ -3,7 +3,9 @@ module TicTacToe::Messageable
     messages = {
       :turn_prompt => "\nIt is #{token}'s turn.\nSelect any available position:",
       :nan => "\nThat is not a valid integer. Please try again.",
-      :not_available => "\nThat is not an available position. Please try again.",
+      :not_available => "\nThat is not an available option. Please try again.",
+      :game_mode_menu => "Please select a game mode:\n  <x/X> Human vs. Human\n  <o/O> Human vs. Computer",
+      :player_menu => "Please select a player:\n  <1> Player 1\n  <2> Player 2"
     }
     messages[msg]
   end
