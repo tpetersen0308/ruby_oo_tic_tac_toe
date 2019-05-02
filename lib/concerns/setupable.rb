a363a5e1aa3c1cf179c2946007883f4cfb793e4a
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-02 13:47:29 -0700

    extract method for selecting a menu option

--- lib/concerns/setupable.rb
+++ lib/concerns/setupable.rb
@@ -1,27 +1,14 @@
 module TicTacToe
   module Setupable
     def self.new_game
-      game_mode = game_mode_selection
-      players = player_selection(game_mode)
+      game_mode = select_menu_option(:game_mode_menu, CONFIG.fetch(:game_mode_options).values)
+      players = select_players(game_mode)
 
       CONFIG.fetch(:game).new(CONFIG.fetch(:board).new, players)
     end
 
     private
-      def self.game_mode_selection
-        CONFIG.fetch(:output).print_message(CONFIG.fetch(:messager).message(:game_mode_menu))
-        selection = CONFIG.fetch(:input).get_input
-        validity = CONFIG.fetch(:validator).validate_input(selection, CONFIG.fetch(:game_mode_options).values)
-        
-        unless validity[:is_valid]
-          CONFIG.fetch(:output).print_message(CONFIG.fetch(:messager).message(:not_available))
-          return game_mode_selection
-        end
-
-        selection
-      end
-
-      def self.player_selection(game_mode)
+      def self.select_players(game_mode)
         if game_mode == CONFIG.fetch(:game_mode_options)[:human_v_human]
           human_v_human_players
         else
@@ -36,18 +23,24 @@ module TicTacToe
       end
 
       def self.human_v_computer_players
-        CONFIG.fetch(:output).print_message(CONFIG.fetch(:messager).message(:player_menu))
-        selection = CONFIG.fetch(:input).get_input.upcase
-        validity = CONFIG.fetch(:validator).validate_input(selection, CONFIG.fetch(:player_options).values)
-      
-        unless validity[:is_valid]
-          CONFIG.fetch(:output).print_message(CONFIG.fetch(:messager).message(:not_available))
-          return human_v_computer_players
-        end
+        selection = select_menu_option(:player_menu, CONFIG.fetch(:player_options).values)
         
         CONFIG.fetch(:player_options).values.map do |token|
           selection == token ? CONFIG.fetch(:players)[:human].new(token) : CONFIG.fetch(:players)[:computer].new(token)
         end
+      end
+
+      def self.select_menu_option(menu, options)
+        CONFIG.fetch(:output).print_message(CONFIG.fetch(:messager).message(menu))
+        selection = CONFIG.fetch(:input).get_input.upcase
+        validity = CONFIG.fetch(:validator).validate_input(selection, options)
+        
+        unless validity[:is_valid]
+          CONFIG.fetch(:output).print_message(CONFIG.fetch(:messager).message(:not_available))
+          return select_menu_option(menu, options)
+        end
+
+        selection
       end
   end
 end
