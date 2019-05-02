1639e957331317e0c2061585b1488fc3eaf79ec1
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-02 11:47:23 -0700

    change namespacing and make all methods class methods

--- lib/concerns/setupable.rb
+++ lib/concerns/setupable.rb
@@ -1,55 +1,57 @@
-module TicTacToe::Setupable
-  def new_game(**config)
-    game_mode = game_mode_selection(config)
-    players = player_selection(game_mode, config)
+module TicTacToe
+  module Setupable
+    def self.new_game(**config)
+      game_mode = game_mode_selection(config)
+      players = player_selection(game_mode, config)
 
-    config[:game].new(config[:board].new, players, config)
+      config[:game].new(config[:board].new, players)
+    end
+
+    private
+      def self.game_mode_selection(**config)
+        config[:output].print_message(config[:messager].message(:game_mode_menu))
+        selection = config[:input].get_input
+        validity = config[:validator].validate_input(selection, config[:game_mode_options].values)
+        
+        unless validity[:is_valid]
+          config[:output].print_message(config[:messager].message(:not_available))
+          return game_mode_selection(config)
+        end
+
+        selection
+      end
+
+      def self.player_selection(game_mode, **config)
+        if game_mode == config[:game_mode_options][:human_v_human]
+          human_v_human_players(config)
+        else
+          human_v_computer_players(config)
+        end
+      end
+
+      def self.human_v_human_players(**config)
+        config[:player_options].values.map do |token| 
+          config[:players][:human].new(token, ui: config[:input])
+        end
+      end
+
+      def self.human_v_computer_players(**config)
+        config[:output].print_message(config[:messager].message(:player_menu))
+        selection = config[:input].get_input.upcase
+        validity = config[:validator].validate_input(selection, config[:player_options].values)
+      
+        unless validity[:is_valid]
+          config[:output].print_message(config[:messager].message(:not_available))
+          return human_v_computer_players(config)
+        end
+        
+        player1_token, player2token = config[:player_options].values
+        human_token, computer_token = selection, selection == player1_token ? player2token : player1_token
+
+        human_player = config[:players][:human].new(human_token, ui: config[:input])
+        computer_player = config[:players][:computer].new(computer_token)
+        
+        selection == player1_token ? [human_player, computer_player] : [computer_player, human_player]
+      end
   end
-
-  private
-    def game_mode_selection(**config)
-      config[:output].print_message(config[:messager].message(:game_mode_menu))
-      selection = config[:input].get_input
-      validity = config[:validator].validate_input(selection, config[:game_mode_options].values)
-      
-      unless validity[:is_valid]
-        config[:output].print_message(config[:messager].message(validity[:msg]))
-        return game_mode_selection(config)
-      end
-
-      selection
-    end
-
-    def player_selection(game_mode, **config)
-      if game_mode == config[:game_mode_options][:human_v_human]
-        human_v_human_players(config)
-      else
-        human_v_computer_players(config)
-      end
-    end
-
-    def human_v_human_players(**config)
-      config[:player_options].values.map do |token| 
-        config[:players][:human].new(token, ui: config[:input])
-      end
-    end
-
-    def human_v_computer_players(**config)
-      config[:output].print_message(config[:messager].message(:player_menu))
-      selection = config[:input].get_input.upcase
-      validity = config[:validator].validate_input(selection, config[:player_options].values)
-    
-      unless validity[:is_valid]
-        config[:output].print_message(config[:messager].message(validity[:msg]))
-        return human_v_computer_players(config)
-      end
-      
-      player1_token, player2token = config[:player_options].values
-      human_token, computer_token = selection, selection == player1_token ? player2token : player1_token
-
-      human_player = config[:players][:human].new(human_token, ui: config[:input])
-      computer_player = config[:players][:computer].new(computer_token)
-      
-      selection == player1_token ? [human_player, computer_player] : [computer_player, human_player]
-    end
 end
---
+++ spec/setupable_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+RSpec.describe TicTacToe::Setupable do
+  before(:each) do
+    @config = {
+      :players => {
+        :human => TicTacToe::HumanPlayer,
+        :computer => TicTacToe::ComputerPlayer,
+      },
+      :board => TicTacToe::Board,
+      :game => TicTacToe::Game,
+      :validator => TicTacToe::Validateable,
+      :messager => TicTacToe::Messageable,
+      :input => TicTacToe::IO::Getable,
+      :output => TicTacToe::IO::Printable,
+      :game_mode_options => {
+        :human_v_human => "1",
+        :human_v_computer => "2",
+      },
+      :player_options => {
+        :player1 => "X",
+        :player2 => "O",
+      }
+    }
+  end
+  
+  it 'can set up a new human vs human game' do
+    allow(@config[:input]).to receive(:gets).and_return("foo", "3\n", "1\n")
+
+    game = TicTacToe::Setupable.new_game(@config)
+    expect(game.current_player.class).to eq(TicTacToe::HumanPlayer)
+    expect(game.next_player.class).to eq(TicTacToe::HumanPlayer)
+    expect(game.board.cells).to eq(Array.new(9))
+  end
+  
+  it 'can set up a new human vs computer game' do
+    allow(@config[:input]).to receive(:gets).and_return("foo", "3\n", "2\n", "1\n", "foo", "o\n")
+
+    game = TicTacToe::Setupable.new_game(@config)
+    expect(game.current_player.class).to eq(TicTacToe::ComputerPlayer)
+    expect(game.next_player.class).to eq(TicTacToe::HumanPlayer)
+    expect(game.board.cells).to eq(Array.new(9))
+  end
+end
