c1acfb2a23fb4de3a562d502e1e7a419bf78d6bc
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-02 15:43:05 -0700

    refactor such that all dependencies are set in initializer

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,18 +1,28 @@
 module TicTacToe
   class HumanPlayer < Player
-    attr_reader :ui
+    attr_reader :ui, :formatter, :messager, :game_status, :selector
     
-    def initialize(token, ui = CONFIG.fetch(:input))
+    def initialize(token, 
+      ui = CONFIG.fetch(:input),
+      formatter = CONFIG.fetch(:formatter),
+      messager = CONFIG.fetch(:messager),
+      game_status = CONFIG.fetch(:game_status),
+      selector = CONFIG.fetch(:selector)
+    )
       super(token)
       self.ui = ui
+      self.formatter = formatter 
+      self.messager = messager 
+      self.game_status = game_status 
+      self.selector = selector 
     end
 
     def move(board_cells)
-      formatted_board = CONFIG.fetch(:formatter).format_board(board_cells)
-      prompt = CONFIG.fetch(:messager).message(:turn_prompt, self.token)
-      available_moves = CONFIG.fetch(:game_status).available_cells(board_cells).map{|cell| (cell += 1).to_s}
-      move = CONFIG.fetch(:selector).select_option(formatted_board + prompt, available_moves)
-      CONFIG.fetch(:formatter).format_move(move)
+      formatted_board = formatter.format_board(board_cells)
+      prompt = messager.message(:turn_prompt, self.token)
+      available_moves = game_status.available_cells(board_cells).map{|cell| (cell += 1).to_s}
+      move = selector.select_option(formatted_board + prompt, available_moves)
+      formatter.format_move(move)
     end
 
     def human?
@@ -20,6 +30,6 @@ module TicTacToe
     end
 
     private
-      attr_writer :ui
+      attr_writer :ui, :formatter, :messager, :game_status, :selector
   end
 end
