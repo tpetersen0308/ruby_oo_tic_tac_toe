e8dd078712da3820c4097d5dd415cf49e158e3c1
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-02 16:26:30 -0700

    refactor to pass dependencies in as method parameters

--- lib/concerns/selectable.rb
+++ lib/concerns/selectable.rb
@@ -1,11 +1,17 @@
 module TicTacToe
   module Selectable
-    def self.select_option(prompt, options)
-      CONFIG.fetch(:output).print_message(prompt)
-      selection = CONFIG.fetch(:input).get_input.upcase
+    def self.select_option(prompt, options, 
+      output = CONFIG.fetch(:output), 
+      input = CONFIG.fetch(:input), 
+      validator = CONFIG.fetch(:validator), 
+      messager = CONFIG.fetch(:messager)
+    )
+
+      output.print_message(prompt)
+      selection = input.get_input.upcase
       
-      unless CONFIG.fetch(:validator).validate_input(selection, options)
-        CONFIG.fetch(:output).print_message(CONFIG.fetch(:messager).message(:not_available))
+      unless validator.validate_input(selection, options)
+        output.print_message(messager.message(:not_available))
         return select_option(prompt, options)
       end
 
