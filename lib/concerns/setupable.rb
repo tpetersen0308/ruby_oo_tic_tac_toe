de73238c9446479338a28f736e4dacec144e40d1
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-02 16:17:10 -0700

    refactor such that dependencies are passed in as arguments

--- lib/concerns/setupable.rb
+++ lib/concerns/setupable.rb
@@ -1,32 +1,32 @@
 module TicTacToe
   module Setupable
-    def self.new_game
-      game_mode_menu = CONFIG.fetch(:messager).message(:game_mode_menu)
-      game_mode = CONFIG.fetch(:selector).select_option(game_mode_menu, CONFIG.fetch(:game_mode_options).values)
+    def self.new_game(option_selector, messager, game_mode_options, game, board, player_options, players)
+      game_mode_menu = messager.message(:game_mode_menu)
+      game_mode = option_selector.select_option(game_mode_menu, game_mode_options.values)
 
       players = case game_mode 
-      when CONFIG.fetch(:game_mode_options).fetch(:human_v_human)
-        setup_players_for_human_v_human_game
-      when CONFIG.fetch(:game_mode_options).fetch(:human_v_computer)
-        setup_players_for_human_v_computer_game
+      when game_mode_options.fetch(:human_v_human)
+        setup_players_for_human_v_human_game(players.fetch(:human), player_options)
+      when game_mode_options.fetch(:human_v_computer)
+        setup_players_for_human_v_computer_game(option_selector, player_options, players, messager)
       end
 
-      CONFIG.fetch(:game).new(CONFIG.fetch(:board).new, players)
+      game.new(board.new, players)
     end
 
     private
-      def self.setup_players_for_human_v_human_game
-        players = CONFIG.fetch(:player_options).values.map do |token| 
-          CONFIG.fetch(:players).fetch(:human).new(token)
+      def self.setup_players_for_human_v_human_game(human_player, player_options)
+        players = player_options.values.map do |token| 
+          human_player.new(token)
         end
       end
 
-      def self.setup_players_for_human_v_computer_game
-        player_menu = CONFIG.fetch(:messager).message(:player_menu)
-        selection = CONFIG.fetch(:selector).select_option(player_menu, CONFIG.fetch(:player_options).values)
+      def self.setup_players_for_human_v_computer_game(option_selector, player_options, players, messager)
+        player_menu = messager.message(:player_menu)
+        selection = option_selector.select_option(player_menu, player_options.values)
         
-        players = CONFIG.fetch(:player_options).values.map do |token|
-          selection == token ? CONFIG.fetch(:players).fetch(:human).new(token) : CONFIG.fetch(:players).fetch(:computer).new(token)
+        players = player_options.values.map do |token|
+          selection == token ? players.fetch(:human).new(token) : players.fetch(:computer).new(token)
         end
       end
   end
--- spec/setupable_spec.rb
+++ spec/setupable_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe TicTacToe::Setupable do
+  before(:each) do
+    @option_selector = TicTacToe::CONFIG.fetch(:option_selector) 
+    @messager = TicTacToe::CONFIG.fetch(:messager) 
+    @game_mode_options = TicTacToe::CONFIG.fetch(:game_mode_options) 
+    @game = TicTacToe::CONFIG.fetch(:game) 
+    @board = TicTacToe::CONFIG.fetch(:board) 
+    @player_options = TicTacToe::CONFIG.fetch(:player_options) 
+    @players = TicTacToe::CONFIG.fetch(:players)
+  end
   
   it 'can set up a new human vs human game' do
     allow(TicTacToe::CONFIG.fetch(:input)).to receive(:gets).and_return("foo", "3\n", "1\n")
 
-    game = TicTacToe::Setupable.new_game
+    game = TicTacToe::Setupable.new_game(@option_selector, @messager, @game_mode_options, @game, @board, @player_options, @players)
     expect(game.current_player.class).to eq(TicTacToe::HumanPlayer)
     expect(game.next_player.class).to eq(TicTacToe::HumanPlayer)
     expect(game.board.cells).to eq(Array.new(9))
@@ -14,7 +23,7 @@ RSpec.describe TicTacToe::Setupable do
   it 'can set up a new human vs computer game' do
     allow(TicTacToe::CONFIG.fetch(:input)).to receive(:gets).and_return("foo", "3\n", "2\n", "1\n", "foo", "o\n")
 
-    game = TicTacToe::Setupable.new_game
+    game = TicTacToe::Setupable.new_game(@option_selector, @messager, @game_mode_options, @game, @board, @player_options, @players)
     expect(game.current_player.class).to eq(TicTacToe::ComputerPlayer)
     expect(game.next_player.class).to eq(TicTacToe::HumanPlayer)
     expect(game.board.cells).to eq(Array.new(9))
