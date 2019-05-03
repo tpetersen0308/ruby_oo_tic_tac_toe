51ed8625366d6837e1d5f4c04e2a2342026a208d
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-03 14:38:22 -0700

    wrap in class << self so methods dont have to be defined explicitly as class methods

--- lib/concerns/checkable.rb
+++ lib/concerns/checkable.rb
@@ -1,57 +1,59 @@
 module TicTacToe
   module Checkable
-    def self.won?(cells)
-      check_row_win(cells) || check_column_win(cells) || check_diagonal_win(cells)
-    end
-
-    def self.cats_game?(cells)
-      cells.compact.length == cells.length && !won?(cells)
-    end
-
-    def self.over?(cells)
-      won?(cells) || cats_game?(cells)
-    end
-
-    def self.available_cells(cells)
-      cells.map.with_index{ |cell, i| i if !cell }.compact
-    end
-    
-    private
-      def self.get_rows(cells)
-        row_size = Math.sqrt(cells.size)
-        cells.each_slice(row_size).to_a
+    class << self
+      def won?(cells)
+        check_row_win(cells) || check_column_win(cells) || check_diagonal_win(cells)
       end
-
-      def self.check_row_win(cells)
-        rows = get_rows(cells)
-        check_combos(rows)
+  
+      def cats_game?(cells)
+        cells.compact.length == cells.length && !won?(cells)
       end
-
-      def self.check_column_win(cells)
-        columns = get_columns(cells)
-        check_combos(columns)
+  
+      def over?(cells)
+        won?(cells) || cats_game?(cells)
       end
-
-      def self.check_diagonal_win(cells)
-        row_size = Math.sqrt(cells.size)
-        nw_to_se_cells = (0..(row_size - 1).to_i).map do |n|
-          cells[n * row_size + n]
+  
+      def available_cells(cells)
+        cells.map.with_index{ |cell, i| i if !cell }.compact
+      end
+      
+      private
+        def get_rows(cells)
+          row_size = Math.sqrt(cells.size)
+          cells.each_slice(row_size).to_a
         end
-        sw_to_ne_cells = (0..(row_size - 1).to_i).map do |n|
-          cells[n * (row_size - 1) + 2]
+  
+        def check_row_win(cells)
+          rows = get_rows(cells)
+          check_combos(rows)
         end
-
-        check_combos([nw_to_se_cells, sw_to_ne_cells])
-      end
-
-      def self.check_combos(combos)
-        combos.any? do |combo|
-          combo.compact.size == combo.size && combo.uniq.size == 1
+  
+        def check_column_win(cells)
+          columns = get_columns(cells)
+          check_combos(columns)
         end
-      end
-
-      def self.get_columns(cells)
-        get_rows(cells).transpose
-      end
+  
+        def check_diagonal_win(cells)
+          row_size = Math.sqrt(cells.size)
+          nw_to_se_cells = (0..(row_size - 1).to_i).map do |n|
+            cells[n * row_size + n]
+          end
+          sw_to_ne_cells = (0..(row_size - 1).to_i).map do |n|
+            cells[n * (row_size - 1) + 2]
+          end
+  
+          check_combos([nw_to_se_cells, sw_to_ne_cells])
+        end
+  
+        def check_combos(combos)
+          combos.any? do |combo|
+            combo.compact.size == combo.size && combo.uniq.size == 1
+          end
+        end
+  
+        def get_columns(cells)
+          get_rows(cells).transpose
+        end
+    end
   end
 end
