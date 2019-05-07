9e956295944292033469b8ff79ea258025a2f75a
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-07 11:22:43 -0700

    extract methods for getting each type of diagonal from check_diagonal_win

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,21 +26,9 @@ module TicTacToe
       end
       
       def check_diagonal_win(board)
-        row_size = board.row_size
-        nw_to_se_cells = (0..(row_size - 1).to_i).map do |n|
-          board.cells[n * row_size + n]
-        end
-        sw_to_ne_cells = (0..(row_size - 1).to_i).map do |n|
-          board.cells[n * (row_size - 1) + 2]
-        end
-        
+        nw_to_se_cells = get_nw_to_se_diagonal(board)
+        sw_to_ne_cells = get_sw_to_ne_diagonal(board)
         check_combos([nw_to_se_cells, sw_to_ne_cells])
-      end
-      
-      def check_combos(combos)
-        combos.any? do |combo|
-          combo.compact.size == combo.size && combo.uniq.size == 1
-        end
       end
       
       def get_rows(board)
@@ -49,6 +37,26 @@ module TicTacToe
       
       def get_columns(board)
         get_rows(board).transpose
+      end
+      
+      def get_nw_to_se_diagonal(board)
+        row_size = board.row_size
+        (0..(row_size - 1).to_i).map do |n|
+          board.cells[n * row_size + n]
+        end
+      end
+      
+      def get_sw_to_ne_diagonal(board)
+        row_size = board.row_size
+        (0..(row_size - 1).to_i).map do |n|
+          board.cells[n * (row_size - 1) + 2]
+        end
+      end
+      
+      def check_combos(combos)
+        combos.any? do |combo|
+          combo.compact.size == combo.size && combo.uniq.size == 1
+        end
       end
     end
   end
