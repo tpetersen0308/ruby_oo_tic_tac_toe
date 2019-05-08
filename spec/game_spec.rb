2f4d35488d901199b53891a69b8df071eac5c1d6
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-08 10:31:25 -0700

    add test for gameplay with lite3 rules

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,77 +12,91 @@ RSpec.describe TicTacToe::Game do
     @board = TicTacToe::Board
   end
 
-  describe "Human vs. Human game" do
+  describe 'Human vs. Human game' do
     before(:each) do
-      @player1 = @human.new("X")
-      @player2 = @human.new("O")
+      @player1 = @human.new('X')
+      @player2 = @human.new('O')
     end
-  
+
     it 'can execute a turn' do
-      board = @board.new(state: ["X", nil, "X", nil, "O", nil, nil, nil, nil])
+      board = @board.new(state: ['X', nil, 'X', nil, 'O', nil, nil, nil, nil])
       game = TicTacToe::Game.new(board, [@player2, @player1])
-      
+
       allow(@game_io).to receive(:gets).and_return("2\n")
-      
+
       board = game.turn
-  
-      expect(board.cells).to eq(["X", "O", "X", nil, "O", nil, nil, nil, nil])
+
+      expect(board.cells).to eq(['X', 'O', 'X', nil, 'O', nil, nil, nil, nil])
     end
-  
+
     it 'can execute a turn with invalid input' do
-      board = @board.new(state: ["X", nil, "X", nil, "O", nil, nil, nil, nil])
+      board = @board.new(state: ['X', nil, 'X', nil, 'O', nil, nil, nil, nil])
       game = TicTacToe::Game.new(board, [@player2, @player1])
-      
-      allow(@game_io).to receive(:gets).and_return("foo", "3\n", "2\n")
-      
+
+      allow(@game_io).to receive(:gets).and_return('foo', "3\n", "2\n")
+
       board = game.turn
-  
-      expect(board.cells).to eq(["X", "O", "X", nil, "O", nil, nil, nil, nil])
+
+      expect(board.cells).to eq(['X', 'O', 'X', nil, 'O', nil, nil, nil, nil])
     end
-  
+
     it 'can execute a game' do
       board = @board.new
       game = TicTacToe::Game.new(board, [@player1, @player2])
-      
+
       allow(@game_io).to receive(:gets).and_return("1\n", "5\n", "3\n", "2\n", "9\n", "8\n")
-      allow(game).to receive(:puts).and_return("")
-      
+      allow(game).to receive(:puts).and_return('')
+
       game = game.play
-  
-      expect(game.board.cells).to eq(["X", "O", "X", nil, "O", nil, nil, "O", "X"])
+
+      expect(game.board.cells).to eq(['X', 'O', 'X', nil, 'O', nil, nil, 'O', 'X'])
       expect(game.current_player).to eq(@player1)
       expect(game.next_player).to eq(@player2)
     end
+
+    it 'can execute a game with Lite3 rules' do
+      board = @board.new
+      game = TicTacToe::Game.new(board, [@player1, @player2])
+
+      allow(@game_io).to receive(:gets).and_return("1\n", "3\n", "7\n", "4\n", "9\n", "8\n", "5\n", "1\n", "6\n", "7\n", "4\n")
+      allow(game).to receive(:puts).and_return('')
+
+      game = game.play
+
+      expect(game.board.cells).to eq(['O', nil, nil, 'X', 'X', 'X', 'O', 'O', nil])
+      expect(game.current_player).to eq(@player2)
+      expect(game.next_player).to eq(@player1)
+    end
   end
 
-  describe "Human vs. Computer game" do
+  describe 'Human vs. Computer game' do
     before(:each) do
-      @player1 = @human.new("X")
-      @player2 = TicTacToe::ComputerPlayer.new("O")
+      @player1 = @human.new('X')
+      @player2 = TicTacToe::ComputerPlayer.new('O')
     end
 
     it 'can execute a turn' do
-      board = @board.new(state: ["X", nil, "X", nil, "O", nil, nil, nil, nil])
+      board = @board.new(state: ['X', nil, 'X', nil, 'O', nil, nil, nil, nil])
       game = TicTacToe::Game.new(board, [@player2, @player1])
-      
+
       allow(@player2).to receive(:move).and_return(1)
-      
+
       board = game.turn
-  
-      expect(board.cells).to eq(["X", "O", "X", nil, "O", nil, nil, nil, nil])
+
+      expect(board.cells).to eq(['X', 'O', 'X', nil, 'O', nil, nil, nil, nil])
     end
 
     it 'can execute a game' do
       board = @board.new
       game = TicTacToe::Game.new(board, [@player1, @player2])
-      
-      allow(@game_io).to receive(:gets).and_return("1\n", "foo", "5\n", "3\n", "bar", "9\n")
+
+      allow(@game_io).to receive(:gets).and_return("1\n", 'foo', "5\n", "3\n", 'bar', "9\n")
       allow(@player2).to receive(:move).and_return(4, 1, 7)
-      allow(game).to receive(:puts).and_return("")
-      
+      allow(game).to receive(:puts).and_return('')
+
       game = game.play
-  
-      expect(game.board.cells).to eq(["X", "O", "X", nil, "O", nil, nil, "O", "X"])
+
+      expect(game.board.cells).to eq(['X', 'O', 'X', nil, 'O', nil, nil, 'O', 'X'])
       expect(game.current_player).to eq(@player1)
       expect(game.next_player).to eq(@player2)
     end
