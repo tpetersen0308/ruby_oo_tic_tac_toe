778701f6cad5601aa5bf8f75b1f9f1597213da2d
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-08 16:58:48 -0700

    remove get prefix from database methods

--- lib/lite_board.rb
+++ lib/lite_board.rb
@@ -10,7 +10,7 @@ module TicTacToe
     end
 
     def update
-      moves = move_database.get_last_n_moves(6)
+      moves = move_database.last_n_moves(6)
       new_cells = Array.new(row_size**2, empty_cell_state)
       moves.each { |move| new_cells[move['position']] = move['player'] }
       self.class.new(move_database: move_database, state: new_cells)
