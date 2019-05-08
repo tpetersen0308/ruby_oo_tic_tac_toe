9ca15ac4dafe36d6128cf0587a81057f30e78545
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-08 16:11:35 -0700

    add menu message for game mode

--- lib/messager.rb
+++ lib/messager.rb
@@ -5,7 +5,8 @@ module TicTacToe
         messages = {
           turn_prompt: "\nIt is #{token}'s turn.\nSelect any available position:",
           not_available: "\nThat is not an available option. Please try again:",
-          player_mode_menu: "\nPlease select a game mode:\n  <1> Human vs. Human\n  <2> Human vs. Computer",
+          game_mode_menu: "\nPlease select a game mode:\n  <1> Tic Tac Toe\n  <2> Lite 3",
+          player_mode_menu: "\nPlease select a player mode:\n  <1> Human vs. Human\n  <2> Human vs. Computer",
           player_menu: "Please select a player:\n  <X> Player 1\n  <O> Player 2",
           winner: "\n#{token} won!",
           cats_game: "\nCat's game!",
