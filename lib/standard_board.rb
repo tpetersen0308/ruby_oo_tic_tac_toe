8ca58012d09ada21b5edd4caaa8b33a04b20cdf8
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-08 14:48:33 -0700

    refactor such that update is a method of a child class of board which queries the DB for move history

---
+++ lib/standard_board.rb
@@ -0,0 +1,23 @@
+require_relative '../config/environment.rb'
+require_relative './board.rb'
+
+module TicTacToe
+  class StandardBoard < Board
+    attr_reader :db
+    def initialize(db: DB[:conn], **kwargs)
+      super(**kwargs)
+      self.db = db
+    end
+
+    def update
+      moves = db.execute('SELECT * FROM moves')
+      new_cells = Array.new(row_size**2, empty_cell_state)
+      moves.each { |move| new_cells[move['position']] = move['player'] }
+      self.class.new(db: db, state: new_cells)
+    end
+
+    private
+
+    attr_writer :db
+  end
+end
