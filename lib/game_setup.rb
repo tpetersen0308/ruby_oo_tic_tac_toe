71d4b8e0954c4f5e55ec16fabfb31d680b2e439e
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-08 14:50:54 -0700

    refactor to create new instance of standard_board on game setup

--- lib/game_setup.rb
+++ lib/game_setup.rb
@@ -2,7 +2,7 @@ require_relative './game_option_selector.rb'
 require_relative './messager.rb'
 require_relative './game_io.rb'
 require_relative './game.rb'
-require_relative './board.rb'
+require_relative './standard_board.rb'
 require_relative './human_player.rb'
 require_relative './computer_player.rb'
 
@@ -10,42 +10,42 @@ module TicTacToe
   module GameSetup
     class << self
       def new_game(
-        option_selector = GameOptionSelector, 
-        messager = Messager, 
+        option_selector = GameOptionSelector,
+        messager = Messager,
         game = Game,
-        board = Board, 
+        standard_board = StandardBoard,
         game_io = GameIO,
         players = {
-          :human => HumanPlayer,
-          :computer => ComputerPlayer
+          human: HumanPlayer,
+          computer: ComputerPlayer
         },
         game_mode_options = {
-          :human_v_human => "1",
-          :human_v_computer => "2" 
-        }, 
+          human_v_human: '1',
+          human_v_computer: '2'
+        },
         player_options = {
-          :player1 => "X",
-          :player2 => "O",
+          player1: 'X',
+          player2: 'O'
         }
       )
-      
+
         game_io.print_message(messager.get_message(:game_mode_menu))
         game_mode = option_selector.select_option(game_mode_options.values)
-  
-        players = case game_mode 
-          when game_mode_options.fetch(:human_v_human)
-            setup_players_for_human_v_human_game(players.fetch(:human), player_options)
-          when game_mode_options.fetch(:human_v_computer)
-            setup_players_for_human_v_computer_game(option_selector, player_options, players, messager, game_io)
+
+        players = case game_mode
+                  when game_mode_options.fetch(:human_v_human)
+                    setup_players_for_human_v_human_game(players.fetch(:human), player_options)
+                  when game_mode_options.fetch(:human_v_computer)
+                    setup_players_for_human_v_computer_game(option_selector, player_options, players, messager, game_io)
           end
-  
-        game.new(board.new, players)
+
+        game.new(standard_board.new, players)
       end
-  
+
       private
-      
+
       def setup_players_for_human_v_human_game(human_player, player_options)
-        players = player_options.values.map do |token| 
+        players = player_options.values.map do |token|
           human_player.new(token)
         end
       end
@@ -53,7 +53,7 @@ module TicTacToe
       def setup_players_for_human_v_computer_game(option_selector, player_options, players, messager, game_io)
         game_io.print_message(messager.get_message(:player_menu))
         selection = option_selector.select_option(player_options.values)
-        
+
         players = player_options.values.map do |token|
           selection == token ? players.fetch(:human).new(token) : players.fetch(:computer).new(token)
         end
