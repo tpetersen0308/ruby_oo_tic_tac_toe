babb23be404cdc3073095c8428ed8c792d7426ef
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-08 14:47:07 -0700

    initialize db connection

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,16 @@ require 'bundler'
 Bundler.require
 
 require_relative '../lib/tic_tac_toe.rb'
+
+DB = { conn: SQLite3::Database.new(':memory:') }.freeze
+
+sql = <<-SQL
+  CREATE TABLE moves (
+    id INTEGER PRIMARY KEY,
+    player TEXT,
+    position INTEGER
+  )
+SQL
+
+DB[:conn].execute(sql)
+DB[:conn].results_as_hash = true
